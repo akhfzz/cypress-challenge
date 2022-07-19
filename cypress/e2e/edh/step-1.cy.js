d1b5b9f1c1269325fadb436bad551f619fcd6caf
akhfzz <akhfzz23@gmail.com>
2022-07-19 16:40:29 +0700

    login-page-authomation

---
+++ cypress/e2e/edh/step-1.cy.js
@@ -0,0 +1,22 @@
+describe('Login to EDH website', function () {
+  before(function () {
+      Cypress.on('uncaught:exception', (err, runnable) => {
+        return false
+      })
+      cy.visit(Cypress.env('URL'))
+      cy.fixture('user').then(function (user) {
+          this.user = user
+      })
+  })
+
+  it('Validate successful Login', function () {
+    cy.get('.mat-form-field-infix > input[name="email"]').type(this.user.email)
+    cy.wait(600)
+    cy.get('.mat-form-field-infix > input[name="password"]').type(this.user.password)
+    cy.get('.mat-form-field-infix > #mat-select-0').click()
+    cy.get('.mat-select-panel > #mat-option-3').click()
+    cy.get('.mat-select-value >.mat-select-placeholder').click()
+    cy.get('.mat-select-panel > #mat-option-6').click()
+    cy.get('.mat-success').click()
+  })
+})
